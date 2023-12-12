bdc42a95ec370c5fb2dcbb7501c3e874a4cd62c0
Quincy <quincy_lim@protonmail.com>
2023-12-12 09:06:10 +0800

    update demo with mongodb

--- demo.py
+++ demo.py
@@ -1,9 +1,12 @@
 from llm import LargeLanguageModel
 from aserious_agent.pandas_agent import create_pandas_dataframe_agent
 from langchain.globals import set_verbose
+from pymongo import MongoClient
 
 import pandas as pd
 
+import datetime
+import pytz
 import yaml
 import fastapi
 import uvicorn
@@ -11,15 +14,19 @@ import uvicorn
 set_verbose(True)
 app = fastapi.FastAPI()
 
+client = MongoClient('quincy.lim-everest.nord', port=27017, username='', password='27017')
+db = client['logs']
+collection = db['de-carton']
+
 with open('./model_configs/neural-chat.yml', 'r') as f:
     model_config = yaml.safe_load(f)
 
-with open('./prompts/pandas_prompt_01.yml', 'r') as f:
+with open('./prompts/pandas_prompt_04.yml', 'r') as f:
     prompt_config = yaml.safe_load(f)
 
 llm = LargeLanguageModel(**model_config, **prompt_config)
 
-df = pd.read_csv('./data/csv_data/sales.csv')
+df = pd.read_csv('./data/csv_data/clean_data.csv')
 dataframe_agent = create_pandas_dataframe_agent(
     llm.llm, 
     df,
@@ -31,13 +38,19 @@ dataframe_agent = create_pandas_dataframe_agent(
     include_df_in_prompt=True,
     return_intermediate_steps=True,
     max_iterations=10,
-    max_execution_time=20,
+    max_execution_time=600,
     early_stopping_method='force', 
 )
 
 @app.post('/chat')
 async def chat(msg):
     result = dataframe_agent({'input': msg})
+    collection.insert_one({
+        'datetime': datetime.datetime.now(pytz.timezone('Asia/Singapore')),
+        'query': msg,
+        'output': result.get('output')
+    })
+
     return result.get('output')
 
 
