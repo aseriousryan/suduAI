42ea70c0771cb58174cdf321faf9c8a882461a32
Ho Rui Xiang <horuixiang@gmail.com>
2024-01-15 00:40:24 +0800

    Update: modify the table ingestion

--- utils/common.py
+++ utils/common.py
@@ -26,24 +26,45 @@ def convert_to_date(df, date_pattern=None):
         except ValueError:
             return date_series
     
+    def clean_column_name(column_name):
+        # Remove dots and replace spaces with underscore
+        return column_name.replace('.', '').replace(' ', '_')
+
     if date_pattern is None:
         date_pattern = r'(\b\d{4}-\d{2}-\d{2}\b)|(\b\d{1,2}/\d{1,2}/\d{2,4}\b)|(\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s\d{1,2},?\s\d{2,4}\b)|(\b(?:Mon(?:day)?|Tue(?:sday)?|Wed(?:nesday)?|Thu(?:rsday)?|Fri(?:day)?|Sat(?:urday)?|Sun(?:day)?)\s(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s\d{1,2},?\s\d{2,4}\b)|(\b\d{2}-\d{2}-\d{4}\b)'
     
-    date_columns = []
+    original_date_columns = []
+    cleaned_date_columns = set()  # To keep track of cleaned columns for dropping the first one
     for column in df.columns:
         if df[column].dtype == 'object' or df[column].dtype == "datetime64[ns]" or df[column].dtype == "str":
             date_matches = df[column].astype(str).str.match(date_pattern, na=False)
-            if date_matches.any(): 
-                df[column] = convert_column_to_date(df[column])
-                date_columns.append(column)
+            if date_matches.any():
+                original_date_columns.append(column)
+                cleaned_column = clean_column_name(column)
+                cleaned_date_columns.add(cleaned_column)
+                df[cleaned_column] = convert_column_to_date(df[column])
     
-    if not date_columns:
+    if not original_date_columns:
         return df
     
-    for date_col in date_columns:
-        df[f"{date_col}_Year"] = df[date_col].dt.year
-        df[f"{date_col}_Month"] = df[date_col].dt.month
-        df[f"{date_col}_Day"] = df[date_col].dt.day
-    
-    return df.drop(columns=date_columns)
+    for date_col in original_date_columns:
+        df[f"{clean_column_name(date_col)}_Year"] = df[clean_column_name(date_col)].dt.year
+        df[f"{clean_column_name(date_col)}_Month"] = df[clean_column_name(date_col)].dt.month
+        df[f"{clean_column_name(date_col)}_Day"] = df[clean_column_name(date_col)].dt.day
+
+    # Drop the first cleaned date column and the original date columns
+    first_cleaned_date_column = next(iter(cleaned_date_columns), None)
+    if first_cleaned_date_column:
+        df.drop(columns=[first_cleaned_date_column] + original_date_columns, inplace=True)
+
+    return df
+
+
+
+
+
+
+
+
+
 
